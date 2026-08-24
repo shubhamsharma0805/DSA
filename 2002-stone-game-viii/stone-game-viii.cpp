6fcd0f5ce7e866933917eb1702dd7eecbb723ad7
shubhamsharma0805 <shubhamsharma08005@gmail.com>
2026-08-24 14:24:29 +0530

    Time: 113 ms (38.65%) | Memory: 87.1 MB (92.39%) - LeetSync

---
+++ 2002-stone-game-viii/stone-game-viii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int stoneGameVIII(vector<int>& stones) {
+        int n = stones.size();
+
+        for(int i = 1; i < n; i++ ){
+            stones[i] += stones[i-1];
+        }
+        int f = stones[n-1];
+
+        for(int i = n-2; i  > 0; i--){
+            f = max(f, stones[i] - f);
+        }
+        return f;
+    }
+};
